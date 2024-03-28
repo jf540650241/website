562370d02a1ce3461c8a291e83cac4d7f1666d92
JF <546050241@qq.com>
2024-03-28 18:00:04 +0800

    修改配置

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://www.deng.quest',
+  url: 'https://docs.singee.xyz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
